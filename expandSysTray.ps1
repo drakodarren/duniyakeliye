4d37911c50cbf6320b44ac0f7c71fa0b496991d4
drakodarren <130096621+drakodarren@users.noreply.github.com>
2023-08-20 13:29:54 -0400

    update expandSysTray.ps1
    sorry i suck at github

--- expandSysTray.ps1
+++ expandSysTray.ps1
@@ -1,3 +1,5 @@
+# Expands the systems tray for Windows 10 1607+. Can be pushed with Intune.
+
 $ErrorActionPreference = 'SilentlyContinue'
 Set-ExecutionPolicy Bypass
 $systrayReg = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer' 
